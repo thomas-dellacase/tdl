a7d19e4c3285001116700b4d741454311065dbb0
thomas-dellacase <thomas.dellacase@laplateforme.io>
2022-05-31 12:01:40 +0200

    start To do

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To do list</title>
+</head>
+<body>
+    <header>
+        <h1>To do list</h1>
+    </header>
+    <main>
+        <article>
+            <h2>What to do?</h2>
+            <form id="newTaskForm">
+                <input type="text" id="newTaskInput" placeholder="What do you need to do?">
+                <input type="submit"
+            </form>
+        </article>
+    </main>
+</body>
+</html>
